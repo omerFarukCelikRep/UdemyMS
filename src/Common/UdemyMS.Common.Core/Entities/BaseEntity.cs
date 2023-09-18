057293ffebca3bfd310bf3cb8f6e307bb685033f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-18 22:27:38 +0300

    Common - Base entity class created

---
+++ src/Common/UdemyMS.Common.Core/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace UdemyMS.Common.Core.Entities;
+
+public class BaseEntity : BaseEntity<int>
+{
+
+}
+
+public class BaseEntity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+}
