4f37b20d01bfdb1aaf9ed03210117c5404107b2a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-20 23:27:41 +0300

    Catalog MS - DatabaseOptions created

---
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.WebApi/Options/DatabaseOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyMS.Microservices.Catalog.WebApi.Options;
+
+public record DatabaseOptions
+{
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Connection { get; set; }
+}
