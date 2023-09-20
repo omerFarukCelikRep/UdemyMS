fe8240dbc46eda1d897d8cf9c19a067e4cf2cede
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-20 23:27:25 +0300

    Catalog MS Options service dependencies added

--- src/Microservices/Catalog/UdemyMS.Microservices.Catalog.WebApi/Extensions/DependencyInjection.cs
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.WebApi/Extensions/DependencyInjection.cs
@@ -1,9 +1,23 @@
-﻿namespace UdemyMS.Microservices.Catalog.WebApi.Extensions;
+﻿using Microsoft.Extensions.Options;
+using UdemyMS.Microservices.Catalog.WebApi.Options;
+
+namespace UdemyMS.Microservices.Catalog.WebApi.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddWebApiServices(this IServiceCollection services)
+    private const string DatabaseSectionName = "Database";
+    public static IServiceCollection AddWebApiServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddOptions(configuration);
+        return services;
+    }
+
+    private static IServiceCollection AddOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<DatabaseOptions>(configuration.GetSection(DatabaseSectionName));
+
+        services.AddSingleton(sp => sp.GetRequiredService<IOptions<DatabaseOptions>>().Value);
+
         return services;
     }
 }
