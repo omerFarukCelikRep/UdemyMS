b4460b73c88fbbbd7c71d972e07fa47716cfaf22
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-20 21:31:27 +0300

    Common Result object added

---
+++ src/Common/UdemyMS.Common.Core/Utilities/Results/Result.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UdemyMS.Common.Core.Utilities.Results;
+public record Result<T>
+{
+    public T? Data { get; init; }
+
+    [JsonIgnore]
+    public int StatusCode { get; init; }
+
+    [JsonIgnore]
+    public bool IsSuccess { get; init; }
+
+    public IReadOnlyList<string> Errors { get; init; } = Enumerable.Empty<string>().ToList();
+
+    public static Result<T> Success(T data, int statusCode) => new()
+    {
+        Data = data,
+        StatusCode = statusCode,
+        IsSuccess = true
+    };
+
+    public static Result<T> Succeess(int statusCode) => new()
+    {
+        StatusCode = statusCode,
+        IsSuccess = true
+    };
+
+    public static Result<T> Error(List<string> errors, int statusCode) => new()
+    {
+        StatusCode = statusCode,
+        Errors = errors.AsReadOnly(),
+        IsSuccess = false
+    };
+
+    public static Result<T> Error(string error, int statusCode) => new()
+    {
+        StatusCode = statusCode,
+        Errors = new List<string>(1) { error }.AsReadOnly(),
+        IsSuccess = false
+    };
+}
