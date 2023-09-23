b58a74f184ea50af5dfd71b80387c9d505054de4
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-24 01:11:52 +0300

    Catalog MS -  Dtos,  CategoryCreateDto added

---
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Entities/Dtos/Categories/CategoryCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using UdemyMS.Microservices.Catalog.Entities.DbSets;
+
+namespace UdemyMS.Microservices.Catalog.Entities.Dtos.Categories;
+public record CategoryCreateDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public static implicit operator Category(CategoryCreateDto categoryCreateDto)
+    {
+        return new Category
+        {
+            Name = categoryCreateDto.Name,
+            CreatedDate = DateTime.Now
+        };
+    }
+}
