9518f714011050943a3a84cd448f7f75e9219d63
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-24 01:10:50 +0300

    Catalog MS - Category entity CategoryListDto implicit opertor added

--- src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Entities/DbSets/Category.cs
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Entities/DbSets/Category.cs
@@ -1,5 +1,14 @@
-﻿namespace UdemyMS.Microservices.Catalog.Entities.DbSets;
+﻿using UdemyMS.Microservices.Catalog.Entities.Dtos.Categories;
+
+namespace UdemyMS.Microservices.Catalog.Entities.DbSets;
 public class Category : BaseEntity<ObjectId>
 {
-    public string Name { get; set; }
-}
+    public string Name { get; set; } = string.Empty;
+
+    public static implicit operator CategoryListDto(Category category) => new()
+    {
+        Id = category.Id.ToString(),
+        Name = category.Name
+    };
+
+   
