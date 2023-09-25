37a76e913f04b8e6f10804cd2fd1cddfa5e4f8a3
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-25 22:54:32 +0300

    Catalog MS  Web Api - Courses Controller added

---
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyMS.Common.Web.Controllers;
+using UdemyMS.Microservices.Catalog.Interfaces.Services;
+
+namespace UdemyMS.Microservices.Catalog.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CoursesController : BaseController
+{
+    private readonly ICourseService _courseService;
+    public CoursesController(ICourseService courseService)
+    {
+        _courseService = courseService;
+    }
+}
