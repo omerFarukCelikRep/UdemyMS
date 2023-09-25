b3d06c5ac842c13d240cd7357fcb2ceb4b1dbd22
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-26 00:46:21 +0300

    Catalog MS  Web Api - Categories Controller create endpoint added

--- src/Microservices/Catalog/UdemyMS.Microservices.Catalog.WebApi/Controllers/CategoriesController.cs
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.WebApi/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UdemyMS.Common.Web.Controllers;
 using UdemyMS.Microservices.Catalog.Interfaces.Services;
+using UdemyMS.Microservices.Catalog.WebApi.Models.Requests.Category;
 
 namespace UdemyMS.Microservices.Catalog.WebApi.Controllers;
 [Route("api/[controller]")]
@@ -25,6 +26,14 @@ public class CategoriesController : BaseController
     public async Task<IActionResult> GetByIdAsync(string id, CancellationToken cancellationToken = default)
     {
         var result = await _categoryService.GetByIdAsync(id, cancellationToken);
+
+        return GetResult(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CategoryCreateRequest request, CancellationToken cancellationToken = default)
+    {
+        var result = await _categoryService.CreateAsync(request, cancellationToken);
 
         return GetResult(result);
     }
