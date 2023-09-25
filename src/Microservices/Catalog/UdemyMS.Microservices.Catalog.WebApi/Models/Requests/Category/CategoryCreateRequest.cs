ec26e73a2dba807b0ea4ccd51822c329b7302b20
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-26 00:36:01 +0300

    Catalog MS  Web Api - Category Create Request added

---
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.WebApi/Models/Requests/Category/CategoryCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using UdemyMS.Microservices.Catalog.Entities.Dtos.Categories;
+
+namespace UdemyMS.Microservices.Catalog.WebApi.Models.Requests.Category;
+
+public class CategoryCreateRequest
+{
+    public string Name { get; set; } = string.Empty;
+
+    public static implicit operator CategoryCreateDto(CategoryCreateRequest request) => new()
+    {
+        Name = request.Name
+    };
+}
