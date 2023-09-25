ba09d1e3da68b13a821b9dc05d201f66cdc318e0
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-26 00:43:54 +0300

    Catalog MS  Web Api - Categories Controller added

---
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyMS.Common.Web.Controllers;
+using UdemyMS.Microservices.Catalog.Interfaces.Services;
+
+namespace UdemyMS.Microservices.Catalog.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : BaseController
+{
+    private readonly ICategoryService _categoryService;
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+}
