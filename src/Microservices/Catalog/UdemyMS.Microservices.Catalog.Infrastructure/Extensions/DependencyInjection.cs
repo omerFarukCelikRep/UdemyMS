59705c0764de300e367a9b1f0a562667906a5642
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-27 00:44:27 +0300

    Catalog MS Infrastructure - Service registrations added

---
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UdemyMS.Microservices.Catalog.Infrastructure.Services;
+using UdemyMS.Microservices.Catalog.Interfaces.Services;
+
+namespace UdemyMS.Microservices.Catalog.Infrastructure.Extensions;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddServices();
+
+        return services;
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<ICourseService, CourseService>();
+
+        return services;
+    }
+}
