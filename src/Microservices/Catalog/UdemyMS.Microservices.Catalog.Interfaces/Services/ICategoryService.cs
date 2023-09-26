80bcd286f30a2a975f761186bc3ee73e74be94f1
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-27 00:19:33 +0300

    Catalog MS Interfaces - ICategoryService interface get all method added

--- src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Interfaces/Services/ICategoryService.cs
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Interfaces/Services/ICategoryService.cs
@@ -5,4 +5,5 @@ namespace UdemyMS.Microservices.Catalog.Interfaces.Services;
 public interface ICategoryService
 {
     Task<Result<CategoryDetailsDto>> CreateAsync(CategoryCreateDto createCategory, CancellationToken cancellationToken = default);
+    Task<Result<List<CategoryListDto>>> GetAllAsync(CancellationToken cancellationToken = default);
 }
