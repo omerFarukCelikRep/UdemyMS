263cb543a8945acdefc9795a4afce7c5e8ccd6d2
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-27 00:16:36 +0300

    Catalog MS Interfaces - ICouseService interface create method added

--- src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Interfaces/Services/ICourseService.cs
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Interfaces/Services/ICourseService.cs
@@ -1,4 +1,8 @@
-﻿namespace UdemyMS.Microservices.Catalog.Interfaces.Services;
+﻿using UdemyMS.Common.Core.Utilities.Results;
+using UdemyMS.Microservices.Catalog.Entities.Dtos.Courses;
+
+namespace UdemyMS.Microservices.Catalog.Interfaces.Services;
 public interface ICourseService
 {
+    Task<Result<CourseDetailsDto>> CreateAsync(CourseCreateDto courseCreate, CancellationToken cancellationToken = default);
 }
