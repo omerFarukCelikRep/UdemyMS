dd0df11485099e252fc7fa3ce0cf80ff8f5d6e18
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-27 00:27:49 +0300

    Catalog MS Infrastructure - CategoryService added

---
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Infrastructure/Services/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using UdemyMS.Microservices.Catalog.Entities.DbSets;
+using UdemyMS.Microservices.Catalog.Interfaces.Options;
+using UdemyMS.Microservices.Catalog.Interfaces.Services;
+
+namespace UdemyMS.Microservices.Catalog.Infrastructure.Services;
+public class CategoryService : ICategoryService
+{
+    private const string CollectionName = "Categories";
+
+    private readonly IMongoCollection<Category> _categories;
+    public CategoryService(IDatabaseOptions databaseOptions)
+    {
+        var client = new MongoClient(databaseOptions.Connection);
+        var database = client.GetDatabase(databaseOptions.Name);
+
+        _categories = database.GetCollection<Category>(CollectionName);
+    }
+}
