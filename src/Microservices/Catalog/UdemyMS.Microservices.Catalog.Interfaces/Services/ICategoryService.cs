43920c5c0c8aacca05717ada73983af3dc69e1b3
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-27 00:19:17 +0300

    Catalog MS Interfaces - ICategoryService interface create method added

--- src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Interfaces/Services/ICategoryService.cs
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Interfaces/Services/ICategoryService.cs
@@ -1,4 +1,8 @@
-﻿namespace UdemyMS.Microservices.Catalog.Interfaces.Services;
+﻿using UdemyMS.Common.Core.Utilities.Results;
+using UdemyMS.Microservices.Catalog.Entities.Dtos.Categories;
+
+namespace UdemyMS.Microservices.Catalog.Interfaces.Services;
 public interface ICategoryService
 {
+    Task<Result<CategoryDetailsDto>> CreateAsync(CategoryCreateDto createCategory, CancellationToken cancellationToken = default);
 }
