dd1e1d8186955ddaee4a28741a382128d2ce418c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-27 00:16:52 +0300

    Catalog MS Interfaces - ICouseService interface delete method added

--- src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Interfaces/Services/ICourseService.cs
+++ src/Microservices/Catalog/UdemyMS.Microservices.Catalog.Interfaces/Services/ICourseService.cs
@@ -5,4 +5,5 @@ namespace UdemyMS.Microservices.Catalog.Interfaces.Services;
 public interface ICourseService
 {
     Task<Result<CourseDetailsDto>> CreateAsync(CourseCreateDto courseCreate, CancellationToken cancellationToken = default);
+    Task<Result> DeleteAsync(string courseId, CancellationToken cancellationToken = default);
 }
