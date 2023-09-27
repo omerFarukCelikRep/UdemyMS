a5af9efb83636277d255cba6a83c9db96215cf5e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-28 01:16:32 +0300

    ExternalServices Identity Server - DbContext Dependency added

---
+++ ExternalServices/UdemyMS.ExternalServices.IdentityServer/Extensions/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyMS.ExternalServices.IdentityServer.Data;
+
+namespace UdemyMS.ExternalServices.IdentityServer.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEFCoreServices(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddEFCoreServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services.AddDbContext<IdentityServerDbContext>(options => options.UseSqlServer(configuration.GetConnectionString(IdentityServerDbContext.Connection)));
+    }
+}
