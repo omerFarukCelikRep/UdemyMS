596f5d8226949ebc1cfa5856f0d2a5b0e6e9f8b8
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-28 01:27:37 +0300

    ExternalServices Identity Server - Dependency identity  server services added

--- ExternalServices/UdemyMS.ExternalServices.IdentityServer/Extensions/DependencyInjection.cs
+++ ExternalServices/UdemyMS.ExternalServices.IdentityServer/Extensions/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Duende.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UdemyMS.ExternalServices.IdentityServer.Data;
 
@@ -9,7 +10,8 @@ public static class DependencyInjection
     public static IServiceCollection AddWebApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddEFCoreServices(configuration)
-                .AddIdentity();
+                .AddCustomIdentity()
+                .AddCustomIdentityServer();
 
         return services;
     }
@@ -19,11 +21,38 @@ public static class DependencyInjection
         return services.AddDbContext<IdentityServerDbContext>(options => options.UseSqlServer(configuration.GetConnectionString(IdentityServerDbContext.Connection)));
     }
 
-    private static IServiceCollection AddIdentity(this IServiceCollection services)
+    private static IServiceCollection AddCustomIdentity(this IServiceCollection services)
     {
         services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<IdentityServerDbContext>()
                 .AddDefaultTokenProviders();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomIdentityServer(this IServiceCollection services)
+    {
+        services.AddIdentityServer()
+            .AddInMemoryClients(new Client[]
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:5002/signout-oidc",
+                    AllowedScopes = { "openid", "profile", "email", "phone" }
+                }
+            })
+            .AddInMemoryIdentityResources(new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResources.Phone(),
+            })
+            .AddAspNetIdentity<IdentityUser>();
 
         return services;
     }
