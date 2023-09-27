209055d859465936680819ef42c2f54a52246d45
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-28 01:38:03 +0300

    Common Web - Base Controller result mehods marked as nonaction

--- src/Common/UdemyMS.Common.Web/Controllers/BaseController.cs
+++ src/Common/UdemyMS.Common.Web/Controllers/BaseController.cs
@@ -4,6 +4,9 @@ using UdemyMS.Common.Core.Utilities.Results;
 namespace UdemyMS.Common.Web.Controllers;
 public class BaseController : ControllerBase
 {
+    [NonAction]
     public IActionResult GetResult(Result result) => new ObjectResult(result);
+
+    [NonAction]
     public IActionResult GetResult<T>(Result<T> result) => new ObjectResult(result) { StatusCode = result.StatusCode };
 }
