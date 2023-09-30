93d02b9a4de5cd8d98e39522f91d309da568d982
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-30 16:11:48 +0300

    ExternalServices Identity Server - UsersController added

---
+++ ExternalServices/UdemyMS.ExternalServices.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UdemyMS.Common.Web.Controllers;
+using UdemyMS.ExternalServices.IdentityServer.Data.DbSets;
+
+namespace UdemyMS.ExternalServices.IdentityServer.Controllers;
+public class UsersController : BaseController
+{
+    private readonly UserManager<AppUser> _userManager;
+    public UsersController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+}
