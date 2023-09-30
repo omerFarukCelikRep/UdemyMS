96827f96377310908e483b1eedc5cfd9c8c0e16c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-09-30 16:12:06 +0300

    ExternalServices Identity Server - UsersController Signup endpoint added

--- ExternalServices/UdemyMS.ExternalServices.IdentityServer/Controllers/UsersController.cs
+++ ExternalServices/UdemyMS.ExternalServices.IdentityServer/Controllers/UsersController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using UdemyMS.Common.Core.Utilities.Results;
 using UdemyMS.Common.Web.Controllers;
 using UdemyMS.ExternalServices.IdentityServer.Data.DbSets;
+using UdemyMS.ExternalServices.IdentityServer.Models.Requests;
 
 namespace UdemyMS.ExternalServices.IdentityServer.Controllers;
 public class UsersController : BaseController
@@ -9,5 +13,21 @@ public class UsersController : BaseController
     public UsersController(UserManager<AppUser> userManager)
     {
         _userManager = userManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SignUpAsync(SignUpRequest request, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var result = await _userManager.CreateAsync(request, request.Password);
+        if (!result.Succeeded)
+        {
+            var errors = result.Errors.Select(x => x.Description)
+                                      .ToList();
+            return BadRequest(Result.Error(errors, (int)HttpStatusCode.BadRequest));
+        }
+
+        return NoContent();
     }
 }
