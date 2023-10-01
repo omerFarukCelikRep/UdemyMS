06a3d6bf65bcec731354423770868a0fbef7ef70
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-01 16:09:26 +0300

    ExternalServices Identity Server - AppUser added as IdentityUser

---
+++ ExternalServices/UdemyMS.ExternalServices.IdentityServer/Data/DbSets/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UdemyMS.ExternalServices.IdentityServer.Data.DbSets;
+
+public class AppUser : IdentityUser
+{
+    public string City { get; set; }
+}
