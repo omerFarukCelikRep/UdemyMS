a3267f767615d2524e4eed7cd8ca944bc1964ad6
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-01 16:13:22 +0300

    ExternalServices Identity Server - SignUpRequest model added

---
+++ ExternalServices/UdemyMS.ExternalServices.IdentityServer/Models/Requests/SignUpRequest.cs
@@ -0,0 +1,18 @@
+﻿using UdemyMS.ExternalServices.IdentityServer.Data.DbSets;
+
+namespace UdemyMS.ExternalServices.IdentityServer.Models.Requests;
+
+public class SignUpRequest
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+
+    public static implicit operator AppUser(SignUpRequest request) => new()
+    {
+        UserName = request.UserName,
+        Email = request.Email,
+        City = request.City,
+    };
+}
