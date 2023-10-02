8b6661c973fb071a83f9c16e7f7dd1a16a78b723
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-02 20:16:00 +0300

    ExternalServices Identity Server - GetUser Response added

---
+++ ExternalServices/UdemyMS.ExternalServices.IdentityServer/Models/Responses/GetUserResponse.cs
@@ -0,0 +1,22 @@
+﻿using UdemyMS.ExternalServices.IdentityServer.Data.DbSets;
+
+namespace UdemyMS.ExternalServices.IdentityServer.Models.Responses;
+
+public class GetUserResponse
+{
+    public string Id { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string? City { get; set; }
+
+    public static implicit operator GetUserResponse(AppUser user)
+    {
+        return new GetUserResponse
+        {
+            Id = user.Id,
+            Username = user.UserName!,
+            Email = user.Email!,
+            City = user.City,
+        };
+    }
+}
