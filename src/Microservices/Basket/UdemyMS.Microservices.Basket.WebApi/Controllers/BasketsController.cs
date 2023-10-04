7df23d47fab21bb7712a073bfb525e862ed68474
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-05 00:26:28 +0300

    Basket MS  Web Api -  Baskets Controller added

---
+++ src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Controllers/BasketsController.cs
@@ -0,0 +1,15 @@
+﻿using UdemyMS.Common.Web.Controllers;
+using UdemyMS.Common.Web.Services;
+using UdemyMS.Microservices.Basket.WebApi.Services;
+
+namespace UdemyMS.Microservices.Basket.WebApi.Controllers;
+public class BasketsController : BaseController
+{
+    private readonly IBasketService _basketService;
+    private readonly IIdentityService _identityService;
+    public BasketsController(IBasketService basketService, IIdentityService identityService)
+    {
+        _basketService = basketService;
+        _identityService = identityService;
+    }
+}
