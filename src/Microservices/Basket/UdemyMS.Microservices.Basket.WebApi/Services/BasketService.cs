bbd7cc90a44e9a0b0c746beb685c5edc3781809e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-06 01:40:05 +0300

    Basket MS  Web Api -  BasketService get methof added

--- src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Services/BasketService.cs
+++ src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Services/BasketService.cs
@@ -1,4 +1,8 @@
-﻿namespace UdemyMS.Microservices.Basket.WebApi.Services;
+﻿using System.Text.Json;
+using UdemyMS.Common.Core.Utilities.Results;
+using UdemyMS.Microservices.Basket.WebApi.Models.Dtos.Baskets;
+
+namespace UdemyMS.Microservices.Basket.WebApi.Services;
 
 public class BasketService : IBasketService
 {
@@ -6,5 +10,19 @@ public class BasketService : IBasketService
     public BasketService(RedisService redisService)
     {
         _redisService = redisService;
+    }
+
+    public async Task<Result<BasketGetDto>> GetAsync(string userId, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var basketAsString = await _redisService.GetDatabase()
+                                                .StringGetAsync(userId);
+        if (string.IsNullOrWhiteSpace(basketAsString))
+            return Result<BasketGetDto>.Error("Basket Not Found", StatusCodes.Status404NotFound); //TODO:Magic string
+
+        var basket = JsonSerializer.Deserialize<BasketGetDto>(basketAsString!);
+
+        return Result<BasketGetDto>.Success(basket, StatusCodes.Status200OK);
     }
 }
--- src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Services/IBasketService.cs
+++ src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Services/IBasketService.cs
@@ -1,5 +1,9 @@
-﻿namespace UdemyMS.Microservices.Basket.WebApi.Services;
+﻿using UdemyMS.Common.Core.Utilities.Results;
+using UdemyMS.Microservices.Basket.WebApi.Models.Dtos.Baskets;
+
+namespace UdemyMS.Microservices.Basket.WebApi.Services;
 
 public interface IBasketService
 {
+    Task<Result<BasketGetDto>> GetAsync(string userId, CancellationToken cancellationToken = default);
 }
