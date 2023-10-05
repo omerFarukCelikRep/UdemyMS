be3673050e8da8452d22a98fc52b7c6743341dbc
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-06 01:00:43 +0300

    Basket MS  Web Api -  BasketCreateRequest added

---
+++ src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Models/Requests/BasketCreateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UdemyMS.Microservices.Basket.WebApi.Models.Requests;
+
+public class BasketCreateRequest
+{
+    public string UserId { get; set; } = string.Empty;
+    public string DiscountCode { get; set; } = string.Empty;
+    public List<BasketItemCreateRequest> BasketItems { get; set; } = new();
+    public decimal TotalPrice => BasketItems.Sum(x => x.Price);
+}
