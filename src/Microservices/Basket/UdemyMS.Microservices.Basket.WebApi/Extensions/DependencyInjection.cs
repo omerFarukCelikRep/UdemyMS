9e32f9e40b92dfd0869aac3dae3b6ee558f00de7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-06 01:00:28 +0300

    Basket MS  Web Api -  depencies added

---
+++ src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Extensions/DependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using UdemyMS.Common.Web.Extensions;
+using UdemyMS.Microservices.Basket.WebApi.Options;
+using UdemyMS.Microservices.Basket.WebApi.Services;
+
+namespace UdemyMS.Microservices.Basket.WebApi.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddRedisServices(configuration)
+                .AddCommonWebServices()
+                .AddScoped<IBasketService, BasketService>()
+                .AddControllers();
+
+        services.AddEndpointsApiExplorer()
+                .AddSwaggerGen();
+
+        return services;
+    }
+
+    private static IServiceCollection AddRedisServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<RedisOptions>(configuration.GetSection(Constants.Redis.ConfigName));
+
+        services.AddSingleton(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<RedisOptions>>().Value;
+
+            var redis = new RedisService(options.Host, options.Port);
+            redis.Connect();
+
+            return redis;
+        });
+
+        return services;
+    }
+}
