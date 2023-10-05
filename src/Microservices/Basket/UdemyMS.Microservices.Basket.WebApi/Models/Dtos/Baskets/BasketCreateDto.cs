2b39185ffdecf50aa69b69f117892441d4f12186
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-06 01:35:38 +0300

    Basket MS  Web Api -  BasketCreateDto added

---
+++ src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Models/Dtos/Baskets/BasketCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UdemyMS.Microservices.Basket.WebApi.Models.Dtos.Baskets;
+
+public class BasketCreateDto
+{
+    public string UserId { get; set; } = string.Empty;
+    public string DiscountCode { get; set; } = string.Empty;
+    public List<BasketItemCreateDto> BasketItems { get; set; } = new();
+    public decimal TotalPrice => BasketItems.Sum(x => x.Price);
+}
