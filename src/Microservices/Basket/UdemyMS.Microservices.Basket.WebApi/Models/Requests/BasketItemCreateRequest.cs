451338d8b9e4852a8065f44d0ae06f4103d2d718
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-06 01:47:30 +0300

    Basket MS  Web Api -  BasketItemCreateRequest implicit operator added

--- src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Models/Requests/BasketItemCreateRequest.cs
+++ src/Microservices/Basket/UdemyMS.Microservices.Basket.WebApi/Models/Requests/BasketItemCreateRequest.cs
@@ -1,8 +1,20 @@
-﻿namespace UdemyMS.Microservices.Basket.WebApi.Models.Requests;
+﻿using UdemyMS.Microservices.Basket.WebApi.Models.Dtos.Baskets;
+
+namespace UdemyMS.Microservices.Basket.WebApi.Models.Requests;
 
 public class BasketItemCreateRequest
 {
-    public string CourseId { get; set; }
-    public string CourseName { get; set; }
+    public string CourseId { get; set; } = string.Empty;
+    public string CourseName { get; set; } = string.Empty;
     public decimal Price { get; set; }
+
+    public static implicit operator BasketItemCreateDto(BasketItemCreateRequest request)
+    {
+        return new()
+        {
+            CourseId = request.CourseId,
+            CourseName = request.CourseName,
+            Price = request.Price
+        };
+    }
 }
