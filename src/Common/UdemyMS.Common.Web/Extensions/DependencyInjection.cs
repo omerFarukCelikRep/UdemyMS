251fc60a19b647fab97cd56bb54f8020be496595
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-06 00:55:53 +0300

    Common Utilities - Identity depency extension added

---
+++ src/Common/UdemyMS.Common.Web/Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UdemyMS.Common.Web.Services;
+
+namespace UdemyMS.Common.Web.Extensions;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCommonWebServices(this IServiceCollection services)
+    {
+        services.AddScoped<IIdentityService, IdentityService>();
+
+        return services;
+    }
+}
