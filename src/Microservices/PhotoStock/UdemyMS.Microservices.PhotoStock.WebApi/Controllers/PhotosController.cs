7b4ea9cfd4093ac371472b9b5612f63945ed370e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-07 22:01:58 +0300

    PhotoStock MS  Web Api -  Photos Controller Save and Delete service call recator

--- src/Microservices/PhotoStock/UdemyMS.Microservices.PhotoStock.WebApi/Controllers/PhotosController.cs
+++ src/Microservices/PhotoStock/UdemyMS.Microservices.PhotoStock.WebApi/Controllers/PhotosController.cs
@@ -1,41 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Net;
 using UdemyMS.Common.Core.Utilities.Results;
 using UdemyMS.Common.Web.Controllers;
 using UdemyMS.Microservices.PhotoStock.WebApi.Models.Responses;
+using UdemyMS.Microservices.PhotoStock.WebApi.Services;
 
 namespace UdemyMS.Microservices.PhotoStock.WebApi.Controllers;
 public class PhotosController : BaseController
 {
+    private readonly IPhotoService _photoService;
+    public PhotosController(IPhotoService photoService)
+    {
+        _photoService = photoService;
+    }
+
     [HttpPost]
     public async Task<IActionResult> SaveAsync(IFormFile file, CancellationToken cancellationToken = default)
     {
-        if (file is null || file.Length <= (long)default)
-            return GetResult(Result.Error("Photo Not Found", (int)HttpStatusCode.BadRequest)); //TODO:Magic string
+        var result = await _photoService.SaveAsync(file, cancellationToken);
 
-        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", file.FileName);
-
-        using var stream = new FileStream(path, FileMode.Create);
-        await file.CopyToAsync(stream, cancellationToken);
-
-        var returnPath = $"photos/{file.FileName}";
-        PhotoSaveResponse response = new() { Url = returnPath };
-
-        return GetResult(Result<PhotoSaveResponse>.Success(response, (int)HttpStatusCode.OK));
+        return GetResult(Result<PhotoSaveResponse>.Success(result.Data!, StatusCodes.Status200OK));
     }
 
     [HttpDelete]
-    public Task<IActionResult> DeleteAsync(string url, CancellationToken cancellationToken = default)
+    public async Task<IActionResult> DeleteAsync(string url, CancellationToken cancellationToken = default)
     {
-        if (cancellationToken.IsCancellationRequested)
-            return Task.FromCanceled<IActionResult>(cancellationToken);
+        var result = await _photoService.DeleteAsync(url, cancellationToken);
 
-        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", url);
-        if (!System.IO.File.Exists(path))
-            return Task.FromResult(GetResult(Result.Error("PhotoNotFound", (int)HttpStatusCode.NotFound)));
-
-        System.IO.File.Delete(path);
-
-        return Task.FromResult(GetResult(Result.Success((int)HttpStatusCode.NoContent)));
+        return GetResult(result);
     }
 }
