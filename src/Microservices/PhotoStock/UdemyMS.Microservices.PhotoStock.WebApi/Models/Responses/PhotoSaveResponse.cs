5606f2f98cbfe2ac4153592ad738e53c0c87b765
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-08 01:58:00 +0300

    PhotoStock MS  Web Api - PhotoSaveResponse implicit opertor added

--- src/Microservices/PhotoStock/UdemyMS.Microservices.PhotoStock.WebApi/Models/Responses/PhotoSaveResponse.cs
+++ src/Microservices/PhotoStock/UdemyMS.Microservices.PhotoStock.WebApi/Models/Responses/PhotoSaveResponse.cs
@@ -1,6 +1,13 @@
-﻿namespace UdemyMS.Microservices.PhotoStock.WebApi.Models.Responses;
+﻿using UdemyMS.Microservices.PhotoStock.WebApi.Models.Dtos;
+
+namespace UdemyMS.Microservices.PhotoStock.WebApi.Models.Responses;
 
 public class PhotoSaveResponse
 {
-    public string Url { get; set; }
+    public string Url { get; set; } = null!;
+
+    public static implicit operator PhotoSaveResponse(PhotoSaveDto photoSaveDto) => new()
+    {
+        Url = photoSaveDto.Url
+    };
 }
