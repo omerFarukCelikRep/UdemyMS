599f341a8adb991325808f5a25ab4c08598f21e3
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-08 17:20:29 +0300

    Common Utilities - Basket MS Constants added

--- src/Common/UdemyMS.Common.Utilities/Constants.cs
+++ src/Common/UdemyMS.Common.Utilities/Constants.cs
@@ -10,12 +10,14 @@ public static class Constants
         {
             public const string Catalog = "resource_catalog";
             public const string PhotoStock = "resource_photo_stock";
+            public const string Basket = "resource_basket";
         }
 
         public struct Permissions
         {
             public const string Catalog = "catalog_fullpermission";
             public const string PhotoStock = "photo_stock_fullpermission";
+            public const string Basket = "basket_fullpermission";
         }
     }
 
