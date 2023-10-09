c985b3e1d49b3159d0c55e694c6c4a6f7067029d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-10 01:24:31 +0300

    Discount MS  Web Api - DiscountUpdateDto created

---
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Models/Dtos/DiscountUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UdemyMS.Microservices.Discount.WebApi.Models.Dtos;
+
+public class DiscountUpdateDto
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public int Rate { get; set; }
+    public string Code { get; set; }
+}
