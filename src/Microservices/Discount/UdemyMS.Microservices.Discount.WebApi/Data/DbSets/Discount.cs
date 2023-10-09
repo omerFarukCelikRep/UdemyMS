2f91d85139b144e9f3db1ce1c663218eca393717
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-10 01:23:31 +0300

    Discount MS  Web Api - Discount entity created

---
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Data/DbSets/Discount.cs
@@ -0,0 +1,10 @@
+﻿using UdemyMS.Common.Core.Entities;
+
+namespace UdemyMS.Microservices.Discount.WebApi.Data.DbSets;
+
+public class Discount : BaseEntity
+{
+    public string UserId { get; set; }
+    public int Rate { get; set; }
+    public string Code { get; set; }
+}
