420b583893f9c1bdd886d15f5b762e8133bf09d3
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-12 00:07:11 +0300

    Discount MS  Web Api -  Discount Service service lifetime dependencies added

--- src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Extensions/DependencyInjection.cs
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Extensions/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using UdemyMS.Microservices.Discount.WebApi.Data.Contexts;
+using UdemyMS.Microservices.Discount.WebApi.Services;
 
 namespace UdemyMS.Microservices.Discount.WebApi.Extensions;
 
@@ -7,8 +8,17 @@ public static class DependencyInjection
     public static IServiceCollection AddWebApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<DapperOptions>(opts => opts.Connection = configuration.GetConnectionString(DapperContext.ConnectionName))
+                .AddScopedServices()
                 .AddEndpointsApiExplorer()
                 .AddSwaggerGen();
+
+        return services;
+    }
+
+    private static IServiceCollection AddScopedServices(this IServiceCollection services)
+    {
+        services.AddScoped<IDapperContext, DapperContext>()
+                .AddScoped<IDiscountService, DiscountService>();
 
         return services;
     }
--- src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Program.cs
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Program.cs
@@ -1,42 +1,23 @@
+using UdemyMS.Microservices.Discount.WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddWebApiServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
+    var forecast = Enumerable.Range(1, 5)
         .ToArray();
     return forecast;
 })
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+.WithName("GetWeatherForecast");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
