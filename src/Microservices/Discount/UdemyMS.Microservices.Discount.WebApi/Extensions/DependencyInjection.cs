261176503e0bec23eef0eceebc210e3a080d8196
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-11 22:40:02 +0300

    Discount MS  Web Api -  Dependency extension added

---
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Extensions/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using UdemyMS.Microservices.Discount.WebApi.Data.Contexts;
+
+namespace UdemyMS.Microservices.Discount.WebApi.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<DapperOptions>(opts => opts.Connection = configuration.GetConnectionString(DapperContext.ConnectionName))
+                .AddEndpointsApiExplorer()
+                .AddSwaggerGen();
+
+        return services;
+    }
+}
