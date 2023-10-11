5b829e1986cd061fba59008042c08a57f17cc640
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-11 23:58:08 +0300

    Discount MS  Web Api -  Dapper Context first or default method added

--- src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Data/Contexts/DapperContext.cs
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Data/Contexts/DapperContext.cs
@@ -4,7 +4,7 @@ using System.Data;
 
 namespace UdemyMS.Microservices.Discount.WebApi.Data.Contexts;
 
-public class DapperContext
+public class DapperContext : IDapperContext
 {
     public const string ConnectionName = "Default";
 
@@ -15,13 +15,20 @@ public class DapperContext
         _connection = new NpgsqlConnection(options.Connection);
     }
 
-    public Task<IEnumerable<T>> QueryAsync<T>(string query, CancellationToken cancellationToken = default)
+    public Task<IEnumerable<T>> QueryAsync<T>(string query, object? param = null, CancellationToken cancellationToken = default)
     {
         ArgumentException.ThrowIfNullOrEmpty(query);
 
         if (cancellationToken.IsCancellationRequested)
             return Task.FromCanceled<IEnumerable<T>>(cancellationToken);
 
-        return _connection.QueryAsync<T>(query);
+        return _connection.QueryAsync<T>(query, param);
+    }
+
+    public async Task<T?> FirstOrDefaultAsync<T>(string query, object? param = null, CancellationToken cancellationToken = default)
+    {
+        var values = await QueryAsync<T>(query, param, cancellationToken);
+
+        return values.FirstOrDefault();
     }
 }
--- src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Data/Contexts/IDapperContext.cs
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Data/Contexts/IDapperContext.cs
@@ -2,5 +2,7 @@
 
 public interface IDapperContext
 {
-
+    Task<bool> ExecuteAsync(string query, object? param = null, CancellationToken cancellationToken = default);
+    Task<T?> FirstOrDefaultAsync<T>(string query, object? param = null, CancellationToken cancellationToken = default);
+    Task<IEnumerable<T>> QueryAsync<T>(string query, object? param = null, CancellationToken cancellationToken = default);
 }
