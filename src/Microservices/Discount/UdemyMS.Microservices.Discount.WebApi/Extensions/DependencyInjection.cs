ac937c3bad5d92e00a185e2577651d32be1deae4
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-15 23:04:23 +0300

    Discount MS  Web Api -  Custom authentication dependency added

--- src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Extensions/DependencyInjection.cs
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Extensions/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿using UdemyMS.Microservices.Discount.WebApi.Data.Contexts;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using UdemyMS.Common.Web.Extensions;
+using UdemyMS.Microservices.Discount.WebApi.Data.Contexts;
 using UdemyMS.Microservices.Discount.WebApi.Services;
 
 namespace UdemyMS.Microservices.Discount.WebApi.Extensions;
@@ -7,9 +12,18 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddWebApiServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.Configure<DapperOptions>(opts => opts.Connection = configuration.GetConnectionString(DapperContext.ConnectionName))
+        var requiredAuthorizationPolicy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .Build();
+
+
+        services.Configure<DapperOptions>(opts => opts.Connection = configuration.GetConnectionString(DapperContext.ConnectionName)!)
+                .AddCommonWebServices()
+                .AddCustomAuthentication(configuration)
                 .AddScopedServices()
-                .AddEndpointsApiExplorer()
+                .AddControllers(opts => opts.Filters.Add(new AuthorizeFilter(requiredAuthorizationPolicy)));
+
+        services.AddEndpointsApiExplorer()
                 .AddSwaggerGen();
 
         return services;
@@ -19,6 +33,21 @@ public static class DependencyInjection
     {
         services.AddScoped<IDapperContext, DapperContext>()
                 .AddScoped<IDiscountService, DiscountService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove(Common.Utilities.Constants.Identity.Claim.Sub);
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opts =>
+                {
+                    opts.Authority = configuration[Common.Utilities.Constants.IdentityServer.ConfigName];
+                    opts.Audience = Common.Utilities.Constants.IdentityServer.Resources.Basket;
+                    opts.RequireHttpsMetadata = false;
+                });
 
         return services;
     }
