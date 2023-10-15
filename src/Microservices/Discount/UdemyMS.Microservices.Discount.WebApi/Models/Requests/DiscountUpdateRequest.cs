f51f5dbb0b9dfdc30db7f15a211fb14a1e2fcc06
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-15 23:04:50 +0300

    Discount MS  Web Api -  DiscountCreateRequest  added

---
+++ src/Microservices/Discount/UdemyMS.Microservices.Discount.WebApi/Models/Requests/DiscountUpdateRequest.cs
@@ -0,0 +1,19 @@
+﻿using UdemyMS.Microservices.Discount.WebApi.Models.Dtos;
+
+namespace UdemyMS.Microservices.Discount.WebApi.Models.Requests;
+
+public class DiscountUpdateRequest
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public int Rate { get; set; }
+    public string Code { get; set; }
+
+    public static implicit operator DiscountUpdateDto(DiscountUpdateRequest request) => new()
+    {
+        Id = request.Id,
+        UserId = request.UserId,
+        Rate = request.Rate,
+        Code = request.Code
+    };
+}
