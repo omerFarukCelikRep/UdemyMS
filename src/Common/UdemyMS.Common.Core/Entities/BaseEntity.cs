2b64577e4a7e6ac57b26c9b703db2485d0b79ef8
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-18 23:34:57 +0300

    Common Core - Base entity equality methods added

--- src/Common/UdemyMS.Common.Core/Entities/BaseEntity.cs
+++ src/Common/UdemyMS.Common.Core/Entities/BaseEntity.cs
@@ -1,12 +1,52 @@
 ﻿namespace UdemyMS.Common.Core.Entities;
 
-public class BaseEntity : BaseEntity<int>
+public abstract class BaseEntity : BaseEntity<int>
 {
 
 }
 
-public class BaseEntity<TId>
+public abstract class BaseEntity<TId> where TId : struct
 {
     public TId Id { get; set; }
     public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    public bool IsTransient() => Id == (TId)default;
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj is not BaseEntity<TId> entity)
+            return false;
+
+        if (ReferenceEquals(this, obj))
+            return true;
+
+        if (GetType() != obj.GetType())
+            return false;
+
+        if (entity.IsTransient() || this.IsTransient())
+            return false;
+
+        return entity.Id == Id;
+    }
+
+    public static bool operator ==(BaseEntity<TId> left, BaseEntity<TId> right)
+    {
+        if (Equals(left, null))
+            return Equals(right, null);
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(BaseEntity<TId> left, BaseEntity<TId> right)
+    {
+        if (Equals(left, null))
+            return !Equals(right, null);
+
+        return !left.Equals(right);
+    }
 }
