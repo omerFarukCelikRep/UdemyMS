482ba579457d9124350f6bb615e1cb85d6ac0a8b
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-20 01:16:21 +0300

    Order MS Domain - Order aggregate added

---
+++ src/Microservices/Order/UdemyMS.Microservices.Order.Domain/Orders/Order.cs
@@ -0,0 +1,35 @@
+﻿using UdemyMS.Common.Core.Aggregates;
+using UdemyMS.Common.Core.Entities;
+using UdemyMS.Microservices.Order.Domain.Orders.Entities;
+using UdemyMS.Microservices.Order.Domain.ValueObjects;
+
+namespace UdemyMS.Microservices.Order.Domain.Orders;
+public class Order : BaseEntity, IAggregateRoot
+{
+    private readonly List<OrderItem> _orderItems;
+
+    public Address Address { get; private set; }
+
+    public string UserId { get; private set; }
+
+    public IReadOnlyCollection<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+    public Order(string userId, Address address)
+    {
+        _orderItems = new List<OrderItem>();
+        UserId = userId;
+        Address = address;
+    }
+
+    public void AddOrderItem(string productId, string productName, decimal price, string thumbnail)
+    {
+        var exist = _orderItems.Exists(x => x.ProductId == productId);
+        if (!exist)
+        {
+            var orderItem = new OrderItem(productId, productName, thumbnail, price);
+            _orderItems.Add(orderItem);
+        }
+    }
+
+    public decimal GetTotalPrice() => _orderItems.Sum(x => x.Price);
+}
