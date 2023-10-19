78be91528fbcae896dbf52d920c5247e76477b43
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-20 01:16:31 +0300

    Order MS Domain - Address value object added

---
+++ src/Microservices/Order/UdemyMS.Microservices.Order.Domain/ValueObjects/Address.cs
@@ -0,0 +1,29 @@
+﻿using UdemyMS.Common.Core.ValueObjects;
+
+namespace UdemyMS.Microservices.Order.Domain.ValueObjects;
+public class Address : ValueObject
+{
+    public string Province { get; }
+    public string District { get; }
+    public string Street { get; }
+    public string ZipCode { get; }
+    public string Line { get; }
+
+    public Address(string province, string district, string street, string zipCode, string line)
+    {
+        Province = province;
+        District = district;
+        Street = street;
+        ZipCode = zipCode;
+        Line = line;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Province;
+        yield return District;
+        yield return Street;
+        yield return ZipCode;
+        yield return Line;
+    }
+}
