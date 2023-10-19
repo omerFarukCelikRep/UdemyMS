006234a308d1720ebbc02c58a9fe91b5d864f05e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-20 01:16:11 +0300

    Order MS Domain - Order Item added

---
+++ src/Microservices/Order/UdemyMS.Microservices.Order.Domain/Orders/Entities/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using UdemyMS.Common.Core.Entities;
+
+namespace UdemyMS.Microservices.Order.Domain.Orders.Entities;
+public class OrderItem : BaseEntity
+{
+    public string ProductId { get; private set; }
+    public string ProductName { get; private set; }
+    public string Thumbnail { get; private set; }
+    public decimal Price { get; private set; }
+
+    public OrderItem(string productId, string productName, string thumbnail, decimal price)
+    {
+        ProductId = productId;
+        ProductName = productName;
+        Thumbnail = thumbnail;
+        Price = price;
+    }
+
+    public void Update(string productName, string thumbnail, decimal price)
+    {
+        ProductName = productName;
+        Thumbnail = thumbnail;
+        Price = price;
+    }
+}
