b75d8407fd2b1a4d72dcff83d17fa83a50e91158
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-23 01:08:10 +0300

    Common Core - Base Repository class added

---
+++ src/Common/UdemyMS.Common.Core/Persistence/EFCore/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using BlogApp.Core.DataAccess.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace UdemyMS.Common.Core.Persistence.EFCore;
+public class BaseRepository<TEntity> : IAsyncRepository where TEntity : class
+{
+    private readonly DbContext _context;
+    private readonly DbSet<TEntity> _table;
+    public BaseRepository(DbContext context)
+    {
+        _context = context;
+        _table = _context.Set<TEntity>();
+    }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
