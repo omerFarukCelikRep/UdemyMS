6dcec001063c8289fcd565a688f59e84dcc9ac65
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-23 01:11:29 +0300

    Order MS Application- Address Dto class added

---
+++ src/Microservices/Order/UdemyMS.Microservices.Order.Application/Dtos/AddressDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UdemyMS.Microservices.Order.Application.Dtos;
+public class AddressDto
+{
+    public string Province { get; }
+    public string District { get; }
+    public string Street { get; }
+    public string ZipCode { get; }
+    public string Line { get; }
+}
