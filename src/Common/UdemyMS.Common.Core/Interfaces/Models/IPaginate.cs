b6b87aed2ab6b95c0b8303ac2e15d17c96328598
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-23 01:07:49 +0300

    Common Core - Paginate model interfaces added

---
+++ src/Common/UdemyMS.Common.Core/Interfaces/Models/IPaginate.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogApp.Core.DataAccess.Interfaces.Models;
+public interface IPaginate<TModel>
+{
+    int Index { get; }
+    int Size { get; }
+    int Count { get; }
+    int Pages { get; }
+    IReadOnlyCollection<TModel> Items { get; }
+    bool HasPrevious { get; }
+    bool HasNext { get; }
+}
