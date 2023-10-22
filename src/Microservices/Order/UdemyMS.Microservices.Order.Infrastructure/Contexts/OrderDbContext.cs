b25a58054cfdf90ea95381ea2939820329d011c1
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-23 01:12:59 +0300

    Order MS Infrastructure - Order DbContext added

---
+++ src/Microservices/Order/UdemyMS.Microservices.Order.Infrastructure/Contexts/OrderDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UdemyMS.Microservices.Order.Infrastructure.Contexts;
+public class OrderDbContext : DbContext
+{
+    private const string DEFAULT_SCHEMA = "ordering";
+    public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
+
+    public DbSet<Domain.Orders.Order> Orders { get; set; }
+    public DbSet<Domain.Orders.Entities.OrderItem> OrderItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Orders.Order>()
+                    .ToTable("Orders", DEFAULT_SCHEMA);
+
+        modelBuilder.Entity<Domain.Orders.Entities.OrderItem>()
+                    .ToTable("OrderItems", DEFAULT_SCHEMA)
+                    .Property(x => x.Price)
+                    .HasColumnType("decimal(18,2)");
+        modelBuilder.Entity<Domain.Orders.Order>()
+                    .OwnsOne(x => x.Address)
+                    .WithOwner();
+    }
+}
