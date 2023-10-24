3f9c65fa93d47c945b563e93450b683809601657
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-25 01:40:17 +0300

    Common Core - IAsyncUpdateableRepository bulk update methods added

--- src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncUpdateableRepository.cs
+++ src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncUpdateableRepository.cs
@@ -1,7 +1,14 @@
-﻿using BlogApp.Core.Entities.Base;
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+using UdemyMS.Common.Core.Entities;
 
 namespace BlogApp.Core.DataAccess.Interfaces.Repositories;
-public interface IAsyncUpdateableRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
+public interface IAsyncUpdateableRepository<TEntity, TId> : IAsyncRepository
+    where TEntity : BaseEntity<TId>
+    where TId : struct
 {
     Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task UpdateRangeAsync(List<TEntity> entities, CancellationToken cancellationToken = default);
+    Task BulkUpdateAsync(Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> predicate, CancellationToken cancellationToken = default);
+    Task BulkUpdateAsync(Expression<Func<TEntity, bool>> predicate, Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> setPropertyCalls, CancellationToken cancellationToken = default);
 }
