f65c121267a80bf6b6b9ad4465a1a30c7f06847c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-25 01:38:02 +0300

    Common Core - IAsyncDeleteableRepository delete methods added

--- src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncDeleteableRepository.cs
+++ src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncDeleteableRepository.cs
@@ -1,7 +1,12 @@
-﻿using BlogApp.Core.Entities.Base;
+﻿using System.Linq.Expressions;
+using UdemyMS.Common.Core.Entities;
 
 namespace BlogApp.Core.DataAccess.Interfaces.Repositories;
-public interface IAsyncDeleteableRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
+public interface IAsyncDeleteableRepository<TEntity, TId> : IAsyncRepository
+    where TEntity : BaseEntity<TId>
+    where TId : struct
 {
     Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(TId id, CancellationToken cancellationToken = default);
+    Task BulkDeleteAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
 }
