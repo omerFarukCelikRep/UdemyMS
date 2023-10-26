61ef4f501939510972f07c6f7d96d49ab9aac8b5
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-27 01:30:08 +0300

    Common Core - IAsyncOrderableRepository generic id type added

--- src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncOrderableRepository.cs
+++ src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncOrderableRepository.cs
@@ -1,8 +1,10 @@
-﻿using BlogApp.Core.Entities.Base;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
+using UdemyMS.Common.Core.Entities;
 
 namespace BlogApp.Core.DataAccess.Interfaces.Repositories;
-public interface IAsyncOrderableRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
+public interface IAsyncOrderableRepository<TEntity, TId> : IAsyncRepository
+    where TEntity : BaseEntity<TId>
+    where TId : struct
 {
     Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, bool tracking = true, CancellationToken cancellationToken = default);
     Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, int takeCount = 0, bool tracking = true, CancellationToken cancellationToken = default);
