c4326899ceb9f4cd2e9c92004c9eda11d17097de
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-27 01:33:11 +0300

    Common Core - IAsyncQueryableRepository generic id type added

--- src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncQueryableRepository.cs
+++ src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncQueryableRepository.cs
@@ -1,8 +1,10 @@
-﻿using BlogApp.Core.Entities.Base;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
+using UdemyMS.Common.Core.Entities;
 
 namespace BlogApp.Core.DataAccess.Interfaces.Repositories;
-public interface IAsyncQueryableRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
+public interface IAsyncQueryableRepository<TEntity, TId> : IAsyncRepository
+    where TEntity : BaseEntity<TId>
+    where TId : struct
 {
     Task<IEnumerable<TEntity>> GetAllAsync(bool tracking = true, CancellationToken cancellationToken = default);
     Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> expression, bool tracking = true, CancellationToken cancellationToken = default);
