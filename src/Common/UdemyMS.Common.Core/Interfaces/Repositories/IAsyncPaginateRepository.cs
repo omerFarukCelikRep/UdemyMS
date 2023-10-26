796162cd4440ede9f506774025b0de342c7b397a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-27 01:30:25 +0300

    Common Core - IAsyncPaginateRepository generic id type added

--- src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncPaginateRepository.cs
+++ src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncPaginateRepository.cs
@@ -1,9 +1,11 @@
 ﻿using BlogApp.Core.DataAccess.Interfaces.Models;
-using BlogApp.Core.Entities.Base;
 using System.Linq.Expressions;
+using UdemyMS.Common.Core.Entities;
 
 namespace BlogApp.Core.DataAccess.Interfaces.Repositories;
-public interface IAsyncPaginateRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
+public interface IAsyncPaginateRepository<TEntity, TId> : IAsyncRepository
+    where TEntity : BaseEntity<TId>
+    where TId : struct
 {
     Task<IPaginate<TEntity>> GetAllAsPaginateAsync(int index = 0, int size = 10, bool tracking = true, CancellationToken cancellationToken = default);
     Task<IPaginate<TEntity>> GetAllAsPaginateAsync(Expression<Func<TEntity, bool>> expression, int index = 0, int size = 10, bool tracking = true, CancellationToken cancellationToken = default);
