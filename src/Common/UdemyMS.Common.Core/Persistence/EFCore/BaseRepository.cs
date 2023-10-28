b7910d89b0e461a0fd133452d50ddf1c4a35252c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-28 23:16:42 +0300

    Common Core - Base Repository update methods added

--- src/Common/UdemyMS.Common.Core/Persistence/EFCore/BaseRepository.cs
+++ src/Common/UdemyMS.Common.Core/Persistence/EFCore/BaseRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BlogApp.Core.DataAccess.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 using UdemyMS.Common.Core.Entities;
 
@@ -7,7 +8,8 @@ namespace UdemyMS.Common.Core.Persistence.EFCore;
 public class BaseRepository<TEntity, TId> :
     IAsyncRepository,
     IAsyncInsertableRepository<TEntity, TId>,
-    IAsyncDeleteableRepository<TEntity, TId>
+    IAsyncDeleteableRepository<TEntity, TId>,
+    IAsyncUpdateableRepository<TEntity, TId>
     where TEntity : BaseEntity<TId>
     where TId : struct
 {
@@ -36,6 +38,17 @@ public class BaseRepository<TEntity, TId> :
                      .ExecuteDeleteAsync(cancellationToken);
     }
 
+    public Task BulkUpdateAsync(Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> setPropertyCalls, CancellationToken cancellationToken = default)
+    {
+        return _table.ExecuteUpdateAsync(setPropertyCalls, cancellationToken);
+    }
+
+    public Task BulkUpdateAsync(Expression<Func<TEntity, bool>> predicate, Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> setPropertyCalls, CancellationToken cancellationToken = default)
+    {
+        return _table.Where(predicate)
+                     .ExecuteUpdateAsync(setPropertyCalls, cancellationToken);
+    }
+
     public Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
     {
         if (cancellationToken.IsCancellationRequested)
@@ -55,5 +68,23 @@ public class BaseRepository<TEntity, TId> :
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         return _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken.IsCancellationRequested)
+            return Task.FromCanceled<TEntity>(cancellationToken);
+
+        var entry = _table.Update(entity);
+        return Task.FromResult(entry.Entity);
+    }
+
+    public Task UpdateRangeAsync(List<TEntity> entities, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken.IsCancellationRequested)
+            return Task.FromCanceled(cancellationToken);
+
+        _table.UpdateRange(entities);
+        return Task.CompletedTask;
     }
 }
