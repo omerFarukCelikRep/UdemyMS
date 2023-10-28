7f04eaf3e326bca96c189bf4f4cfb1b2c782a050
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-28 23:14:53 +0300

    Common Core - Base Repository add methods added

--- src/Common/UdemyMS.Common.Core/Persistence/EFCore/BaseRepository.cs
+++ src/Common/UdemyMS.Common.Core/Persistence/EFCore/BaseRepository.cs
@@ -1,8 +1,13 @@
 ﻿using BlogApp.Core.DataAccess.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
+using UdemyMS.Common.Core.Entities;
 
 namespace UdemyMS.Common.Core.Persistence.EFCore;
-public class BaseRepository<TEntity> : IAsyncRepository where TEntity : class
+public class BaseRepository<TEntity, TId> :
+    IAsyncRepository,
+    IAsyncInsertableRepository<TEntity, TId>
+    where TEntity : BaseEntity<TId>
+    where TId : struct
 {
     private readonly DbContext _context;
     private readonly DbSet<TEntity> _table;
@@ -12,8 +17,20 @@ public class BaseRepository<TEntity> : IAsyncRepository where TEntity : class
         _table = _context.Set<TEntity>();
     }
 
+    public async Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        var entry = await _table.AddAsync(entity, cancellationToken);
+        return entry.Entity;
+    }
+
+    public Task AddRangeAsync(List<TEntity> entities, CancellationToken cancellationToken = default)
+    {
+        return _table.AddRangeAsync(entities, cancellationToken);
+    }
+
+
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return _context.SaveChangesAsync(cancellationToken);
     }
 }
