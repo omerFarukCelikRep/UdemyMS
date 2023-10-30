4f43b10eb7d3d50da2e428205357746c67b3a6e4
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-30 23:10:33 +0300

    Common Core - IAsyncOrderableRepository return type changed

--- src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncOrderableRepository.cs
+++ src/Common/UdemyMS.Common.Core/Interfaces/Repositories/IAsyncOrderableRepository.cs
@@ -6,8 +6,8 @@ public interface IAsyncOrderableRepository<TEntity, TId> : IAsyncRepository
     where TEntity : BaseEntity<TId>
     where TId : struct
 {
-    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, bool tracking = true, CancellationToken cancellationToken = default);
-    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, int takeCount = 0, bool tracking = true, CancellationToken cancellationToken = default);
-    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, bool tracking = true, CancellationToken cancellationToken = default);
-    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, int takeCount = 0, bool tracking = true, CancellationToken cancellationToken = default);
+    Task<IQueryable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, bool tracking = true, CancellationToken cancellationToken = default);
+    Task<IQueryable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, int takeCount = 0, bool tracking = true, CancellationToken cancellationToken = default);
+    Task<IQueryable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, bool tracking = true, CancellationToken cancellationToken = default);
+    Task<IQueryable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, int takeCount = 0, bool tracking = true, CancellationToken cancellationToken = default);
 }
