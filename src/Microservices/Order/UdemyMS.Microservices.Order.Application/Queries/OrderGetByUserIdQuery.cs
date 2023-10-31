f12c4a7f4c824dee8dd58942781a12177637106a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-10-31 23:16:12 +0300

    Order MS Application- Order By User Id query added

---
+++ src/Microservices/Order/UdemyMS.Microservices.Order.Application/Queries/OrderGetByUserIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using UdemyMS.Common.Core.Utilities.Results;
+using UdemyMS.Microservices.Order.Application.Dtos;
+
+namespace UdemyMS.Microservices.Order.Application.Queries;
+public class OrderGetByUserIdQuery : IRequest<Result<List<OrderDto>>>
+{
+    public string UserId { get; set; }
+}
---
+++ src/Microservices/Order/UdemyMS.Microservices.Order.Application/Queries/OrderGetByUserIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using UdemyMS.Common.Core.Utilities.Results;
+using UdemyMS.Microservices.Order.Application.Dtos;
+using UdemyMS.Microservices.Order.Infrastructure.Contexts;
+
+namespace UdemyMS.Microservices.Order.Application.Queries;
+public class OrderGetByUserIdQueryHandler : IRequestHandler<OrderGetByUserIdQuery, Result<List<OrderDto>>>
+{
+    private readonly OrderDbContext _context;
+    public OrderGetByUserIdQueryHandler(OrderDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Result<List<OrderDto>>> Handle(OrderGetByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _context.Orders
+            .Include(x => x.OrderItems)
+            .Where(x => x.UserId == request.UserId)
+            .Select(x => new OrderDto(x.Id, x.CreatedDate, x.Address, x.UserId, x.OrderItems
+                .Select(oi => new OrderItemDto(oi.ProductId, oi.ProductName, oi.Thumbnail, oi.Price
+                ))
+                .ToList()))
+            .ToListAsync(cancellationToken);
+
+        return Result<List<OrderDto>>.Success(result, (int)HttpStatusCode.OK);
+    }
+}
