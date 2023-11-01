0c75ee20ce563da40941f8db68f877404a494e3d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-11-02 00:52:50 +0300

    Order MS Application- Order Create Command added

---
+++ src/Microservices/Order/UdemyMS.Microservices.Order.Application/Commands/OrderCreateCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using UdemyMS.Common.Core.Utilities.Results;
+using UdemyMS.Microservices.Order.Application.Dtos;
+
+namespace UdemyMS.Microservices.Order.Application.Commands;
+public class OrderCreateCommand : IRequest<Result<CreatedOrderDto>>
+{
+    public string UserId { get; set; }
+    public AddressDto Address { get; set; }
+    public List<OrderItemDto> Orders { get; set; }
+}
