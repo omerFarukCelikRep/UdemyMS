2282b2f9416d690d1003909d31d379a2f6bd6306
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-11-18 22:25:02 +0300

    projects migrated to dotnet 8

---
+++ src/Common/UdemyMS.Common.Utilities/Mapper/ObjectMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace UdemyMS.Common.Utilities.Mapper;
+public static class ObjectMapper
+{
+    //private static readonly Lazy<IMapper> _lazy = new(() =>
+    //{
+    //    var config = new MapperConfiguration(opts => opts.AddProfile());
+    //    return config.CreateMapper();
+    //});
+
+    //public static IMapper Mapper => _lazy.Value;
+}
